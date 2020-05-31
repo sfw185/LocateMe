ba90ac3badc02ebb6bfbfc0ac6440b7f7f149826
Glitch (locate-me) <none>
2020-05-31 00:50:35 +0000

    Adding redis

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const http = require("http");
 const jade = require("pug");
 const template = jade.compileFile("template.pug");
+const redis_client = require('redis').createClient(process.env.REDIS_URL);
+
+const LOCATION_KEY = "LOCATION";
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((request, response) => {
   if (request.path === "/update") {
-    console.log("Update redis here");
+    redis_client.set("LOCATION", LOCATION_KEY);
   }
 
   response.statusCode = 200;
   response.setHeader("Content-Type", "text/html");
-  response.end(template({ location: "blah,blah" }));
+  response.end(template({ location: redis_client.get(LOCATION_KEY) }));
 });
 
 server.listen(port, null, () => {
