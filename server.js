d4c2db2cd6695713b77b92d0781a9e021ea90fee
Simon Wardan <simon@wardan.com>
2020-05-31 12:21:00 +1000

    Working get/set on location

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
 const http = require("http");
+const url = require("url");
 const pug = require("pug");
 
 const redis = require("redis");
 const redis_client = redis.createClient(process.env.REDIS_URL);
+
 const { promisify } = require("util");
 const getAsync = promisify(redis_client.get).bind(redis_client);
 const setAsync = promisify(redis_client.set).bind(redis_client);
 
-redis_client.on("error", function(error) {
-  console.error(error);
-});
-
 const LOCATION_KEY = "LOCATION";
-
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
   response.setHeader("Content-Type", "text/html");
-  const path = request.path;
   
-  if (path == "/update") {
-    setAsync(LOCATION_KEY, "testing").then(() => {
-      response.end(pug.renderFile("template.pug", { path, location: "updated" }));
+  const params = new URL(request.url, "https://dummy.site/").searchParams;
+  const location = params.get("loc");
+  const password = params.get("pass");
+
+  if (request.url.startsWith("/update") && password == process.env.PASSWORD) {
+    setAsync(LOCATION_KEY, location).then(() => {
+      console.log(`Location has been updated to ${location}`)
+      response.end(pug.renderFile("template.pug", { location: "updated" }));
+    });
+  } else {
+    getAsync(LOCATION_KEY).then((location) => {
+      response.end(pug.renderFile("template.pug", { location }));
     });
   }
-  
-  getAsync(LOCATION_KEY).then((location) => {
-    response.end(pug.renderFile("template.pug", { path, location }));
-  })
 });
 
 server.listen(port, null, () => {
