78f0cc12be29fcd0ec31cb77ff41a081f5d65d4a
Glitch (locate-me) <none>
2020-05-31 00:13:02 +0000

    Rewound to commit 28a3d7630d63c3ec0f8fb830a77ca277c54509f8 + 0 edits

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, null, () => {
+  console.log(`Server running at ${port}`);
+});
