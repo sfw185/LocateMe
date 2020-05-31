f6574453bb3a5391b35faaa7a43751790f8ba05d
Glitch (locate-me) <none>
2020-05-31 00:39:18 +0000

    🔋🎡 Checkpoint ./server.js:181774/1338 ./index.jade:181774/154 ./template.jade:181774/47

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const http = require("http");
+const jade = require("jade");
+const template = jade.compileFile("template.jade");
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+const server = http.createServer((request, response) => {
+  if (request.path === "/update") {
+    console.log("Update redis here");
+  }
+
+  response.statusCode = 200;
+  response.setHeader("Content-Type", "text/html");
+  response.end(template({ location: "blah,blah" }));
 });
 
 server.listen(port, null, () => {
